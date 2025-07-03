494998a4aeb9e6dba761401ed495b3cd4c5ffbb3
unlimitedloop-admin <atomic-u7.effixy@loopunlimited-rootone.com>
2025-07-03 19:59:57 +0900

    [mm2hack] Refactored codebase.

--- RetroEngine/mm2hack/resource.h
+++ RetroEngine/mm2hack/resource.h
@@ -4,30 +4,23 @@
 //
 #define IDR_MAINMENU                    101
 #define IDI_WNDICON                     102
-#define ID_40001                        40001
-#define ID_40002                        40002
 #define ID_Menu                         40003
-#define ID_40004                        40004
-#define ID_40005                        40005
-#define ID_40006                        40006
 #define ID_FILE_START                   40007
 #define ID_FILE_RESET                   40008
 #define ID_FILE_STOP                    40009
 #define ID_FILE_START_DEBUG             40010
 #define ID_APP_EXIT                     40011
-#define ID_DEBUG_40012                  40012
-#define ID_40013                        40013
 #define ID_SCRIPT_001                   40014
-#define ID_40015                        40015
-#define ID_40016                        40016
-#define ID_40017                        40017
+#define ID_MENU_C                       40018
+#define ID_MENU_T                       40019
+#define ID_MENU_H                       40020
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
-#define _APS_NEXT_COMMAND_VALUE         40018
+#define _APS_NEXT_COMMAND_VALUE         40021
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
