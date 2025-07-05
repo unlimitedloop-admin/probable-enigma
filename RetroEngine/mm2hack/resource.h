df69b5ce6b4ec4170016891a9496981c3663028c
unlimitedloop-admin <atomic-u7.effixy@loopunlimited-rootone.com>
2025-07-05 18:22:18 +0900

    [mm2hack] Added a menu command for the save system.

--- RetroEngine/mm2hack/resource.h
+++ RetroEngine/mm2hack/resource.h
@@ -4,23 +4,38 @@
 //
 #define IDR_MAINMENU                    101
 #define IDI_WNDICON                     102
-#define ID_Menu                         40003
-#define ID_FILE_START                   40007
-#define ID_FILE_RESET                   40008
-#define ID_FILE_STOP                    40009
-#define ID_FILE_START_DEBUG             40010
-#define ID_APP_EXIT                     40011
-#define ID_SCRIPT_001                   40014
-#define ID_MENU_C                       40018
-#define ID_MENU_T                       40019
-#define ID_MENU_H                       40020
+#define ID_Menu                         40001
+#define ID_FILE_START                   40002
+#define ID_FILE_RESET                   40003
+#define ID_FILE_STOP                    40004
+#define ID_FILE_START_DEBUG             40005
+#define ID_APP_EXIT                     40006
+#define ID_SCRIPT_001                   40007
+#define ID_MENU_C                       40008
+#define ID_MENU_T                       40009
+#define ID_MENU_H                       40010
+#define ID_FILE_SAVE                    40011
+#define ID_FILE_LOAD                    40012
+#define ID_SLOT_0                       40013
+#define ID_SLOT_1                       40014
+#define ID_SLOT_2                       40015
+#define ID_SLOT_3                       40016
+#define ID_SLOT_4                       40017
+#define ID_SLOT_5                       40018
+#define ID_SLOT_6                       40019
+#define ID_SLOT_7                       40020
+#define ID_SLOT_8                       40021
+#define ID_SLOT_9                       40022
+#define ID_SLOT_EMPTY                   40023
+#define ID_SLOT_NEXT                    40024
+#define ID_SLOT_PREVIOUS                40025
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
-#define _APS_NEXT_COMMAND_VALUE         40021
+#define _APS_NEXT_COMMAND_VALUE         40026
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
